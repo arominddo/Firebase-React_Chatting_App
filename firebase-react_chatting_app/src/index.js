f354e5b9dfc926676f96af408662b341348940b8
mingddo <godhsb@naver.com>
2023-12-22 16:41:52 +0900

    중간 커밋

--- firebase-react_chatting_app/src/index.js
+++ firebase-react_chatting_app/src/index.js
@@ -6,25 +6,28 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 
-import { createStore, applyMiddleware} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
 
 import Reducer from "./redux/reducer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-    <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-      <App />
-    </Provider>
-    
-  </React.StrictMode>
+    <React.StrictMode>
+
+        <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+
+    </React.StrictMode>
 );
 
 reportWebVitals();
