fb35cd57687e42c31b609de385208c4a24193861
mingddo <godhsb@naver.com>
2024-12-16 10:40:27 +0900

    Update : DM 목록 UI 구현

--- src/pages/ChatPage/SidePanel/DirectMessages.jsx
+++ src/pages/ChatPage/SidePanel/DirectMessages.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { onChildAdded, ref as dbRef } from "firebase/database";
+import { FaRegSmile } from "react-icons/fa";
+import { db } from "../../../firebase";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const DirectMessages = () => {
-  return (
-    <div>DirectMessages</div>
-  )
-}
+    
+    const usersRef = dbRef(db, "users");
 
-export default DirectMessages
+    const [users, setUsers] = useState([]);
+    const [activeChatRoomId, setActiveChatRoomId] = useState("");
+
+    const {currentUser} = useSelector(state => state.user);
+
+    useEffect(() => {
+        
+        if(currentUser?.uid){
+            addUsersListener(currentUser.uid)
+        }
+        
+        return () => {
+
+        }
+    }, [currentUser?.uid])
+
+    const addUsersListener = (currentUserId) => {
+        let usersArray = [];
+
+        onChildAdded(usersRef, DataSnapshot => {
+            if(currentUserId !== DataSnapshot.key){
+                let user = DataSnapshot.val();
+                user["uid"] = DataSnapshot.key;
+                usersArray.push(user);
+
+                const newUsersArray = [...usersArray];
+                setUsers(newUsersArray);
+            }
+        })
+    }
+
+    const renderDirectMessages = users => {
+        return (
+            users.length > 0 &&
+            users.map(user => (
+                <li key={user.uid}
+                style={{
+                    backgroundColor : user.uid === activeChatRoomId ? "#ffffff45" : ""
+                }}
+                >
+                    # {user.name}
+                </li>
+            )    
+            )
+
+        )
+        
+    }
+    
+    return (
+        <div>
+            <span style={{ display : "flex", alignItems : "center" }}>
+                <FaRegSmile style={{ marginRight: 3 }} />
+                DIRECT MESSAGES
+            </span>
+            <ul style={{listStyleType : "none", padding : 0}}>
+                {renderDirectMessages(users)}
+            </ul>
+        </div>
+    );
+};
+
+export default DirectMessages;
